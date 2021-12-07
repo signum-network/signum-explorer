0a67759b8350be727ba4420570f368a05a0cf044
pir8radio <pir8radio@gmail.com>
2021-12-07 00:08:05 -0600

    Update peers.py
    Sleep has to go at the top of the script or data gets delayed by the sleep amount.    if sleep is at the bottom of the script, submitted data is always twice the sleep timer behind.

--- scan/peers.py
+++ scan/peers.py
@@ -27,6 +27,10 @@ from scan.models import PeerMonitor
 
 logger = logging.getLogger(__name__)
 
+if PEERS_SCAN_DELAY > 0:
+    logger.info(f"Sleeping for {PEERS_SCAN_DELAY} seconds...")
+sleep(PEERS_SCAN_DELAY)
+
 def get_ip_by_domain(peer: str) -> str or None:
     # truncating port if exists
     if not peer.startswith("http"):
@@ -280,9 +284,5 @@ def peer_cmd():
         availability=100 - (F("downtime") / F("lifetime") * 100),
         modified_at=timezone.now(),
     )
-    if PEERS_SCAN_DELAY > 0:
-        logger.info(f"Sleeping for {PEERS_SCAN_DELAY} seconds...")
-    logger.info("Done")
-    sleep(PEERS_SCAN_DELAY)
-
+logger.info("Done")
     
