1d36b858e2838432a569baf6d5f30d78a65b73c7
frankTheTank72 <39116221+frankTheTank72@users.noreply.github.com>
2022-03-18 19:55:34 +0100

    Geo-Update
    Change API

--- scan/peers.py
+++ scan/peers.py
@@ -54,7 +54,7 @@ def get_ip_by_domain(peer: str) -> str or None:
 @cache_memoize(60 * 60 * 24 * 7)
 def get_country_by_ip(ip: str) -> str:
     try:
-        response = requests.get(f"http://www.geoplugin.net/json.gp?ip={ip}")
+        response = requests.get(f"http://ip-api.com/json/{ip}")
         response.raise_for_status()
         json_response = response.json()
         return json_response["geoplugin_countryCode"] or "??"
