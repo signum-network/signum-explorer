e2a7f1dfc193e492b0b7f250b52713b14ae7e678
frankTheTank72 <39116221+frankTheTank72@users.noreply.github.com>
2022-08-10 19:02:59 +0200

    Add Amount to trades view
    new function multiply and footer year 2022

--- scan/templatetags/burst_tags.py
+++ scan/templatetags/burst_tags.py
@@ -519,3 +519,10 @@ def smooth_timedelta(timedelta_obj):
         time_str = f"{int(secs)} seconds"
 
     return time_str
+
+@register.simple_tag()
+def multiply(qty, unit_price, direction, *args, **kwargs):
+    print(qty)
+    print(unit_price)
+    print(direction)
+    return float(qty) * float(unit_price) * float(direction)
