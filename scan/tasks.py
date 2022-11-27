7e5d8f344decce0869b0a5a08232701b6dddda23
pir8radio <pir8radio@gmail.com>
2022-11-26 21:18:33 -0600

    Update tasks.py
    removed celery tasks, and made tasks.py its own module like peers.py     you will now need to either add this script to supervisord config or just run it every 60 seconds using a bash script or something else..  like this:     python3 manage.py tasks

--- scan/tasks.py
+++ scan/tasks.py
@@ -1,50 +1,30 @@
+import logging
+import os
+
+from time import sleep
 from django.conf import settings
-
-from config import celery_app
+from config.settings import TASKS_SCAN_DELAY
 from scan.caching_data.exchange import CachingExchangeData
-from scan.caching_data.pending_txs import CachingPendingTxs
-from scan.caching_data.total_accounts_count import CachingTotalAccountsCount
-from scan.caching_data.total_burst_circulation import CachingTotalBurstCirculation
-from scan.caching_data.total_txs_count import CachingTotalTxsCount
 
+#from scan.caching_data.total_accounts_count import CachingTotalAccountsCount
+#from scan.caching_data.total_txs_count import CachingTotalTxsCount
 
-@celery_app.on_after_finalize.connect
-def setup_periodic_tasks(sender, **kwargs):
-    if not settings.TEST_NET:
-        sender.add_periodic_task(60, update_cache_exchange_data)
+logger = logging.getLogger(__name__)
 
-    sender.add_periodic_task(10, update_cache_pending_txs)
-
-    sender.add_periodic_task(3000, update_cache_total_txs_count)
-    update_cache_total_txs_count.apply_async(countdown=15)
-
-    sender.add_periodic_task(3300, update_cache_total_accounts_count)
-    update_cache_total_accounts_count.apply_async(countdown=45)
-
-    sender.add_periodic_task(43200, update_cache_total_burst_circulation)
-    update_cache_total_burst_circulation.apply_async(countdown=60)
-
-
-@celery_app.task(time_limit=10)
-def update_cache_exchange_data():
+def task_cmd():
+    if TASKS_SCAN_DELAY > 0:
+        logger.info(f"Tasks Sleeping for {TASKS_SCAN_DELAY} seconds...")
+    sleep(TASKS_SCAN_DELAY)
+    logger.info("TASK - Update Cache Exchange data")
     CachingExchangeData().update_live_data()
 
 
-@celery_app.task(time_limit=10)
-def update_cache_pending_txs():
-    CachingPendingTxs().update_live_data()
+
+#    def update_cache_total_txs_count():
+#        logger.info("TASK - Update Total TX's count data")
+#        CachingTotalTxsCount().update_live_data()
 
 
-@celery_app.task(time_limit=15)
-def update_cache_total_txs_count():
-    CachingTotalTxsCount().update_live_data()
-
-
-@celery_app.task(time_limit=15)
-def update_cache_total_burst_circulation():
-    CachingTotalBurstCirculation().update_live_data()
-
-
-@celery_app.task(time_limit=15)
-def update_cache_total_accounts_count():
-    CachingTotalAccountsCount().update_live_data()
+#    def update_cache_total_accounts_count():
+#        logger.info("TASK - Update Total Accounts data")
+#        CachingTotalAccountsCount().update_live_data()
