5dc71fc90d6b971e83aa5371db0909a882382c58
pir8radio <pir8radio@gmail.com>
2022-11-28 13:47:13 -0600

    Update settings.py
    json not needed, changed to list like all other options in the .env

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "off") == "on"
 
-INTERNAL_IPS = json.loads(os.environ.get("DEBUG_TOOLBAR_INTERNAL_IPS", "[]"))
+INTERNAL_IPS = os.environ.get("DEBUG_TOOLBAR_INTERNAL_IPS", "[]")
 
 ALLOWED_HOSTS = ["*"]
 
