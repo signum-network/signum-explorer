204c2584cd213f12eb0797d066547d7048acef1c
pir8radio <pir8radio@gmail.com>
2022-11-29 16:12:58 -0600

    Update transactions.py
    old method was expecting the message to be in a specific location...   this new method just strips the bytes out of the message...    I will continue to test, but this is now working on really old messages (didnt before) like this one: https://explorer.notallmine.net/tx/2465240108500726185   as well as new messages like this one: https://explorer.notallmine.net/tx/11289032442914210915   and also now works on other transaction types like subscriptions (didnt before): https://explorer.notallmine.net/tx/15380928695827209893

--- burst/libs/transactions.py
+++ burst/libs/transactions.py
@@ -1,10 +1,3 @@
 def get_message(attachment: bytes) -> str:
-    """
-    byte[0] - tx version
-    2**8 * byte[2] + byte[1] - message len
-    byte[3] and byte[4] - ?
-    """
-    header = attachment[:5]
-    body = attachment[5:]
-    message_len = 2 ** 8 * header[2] + header[1]
-    return body[:message_len].decode()
+    txt = str(attachment).split('\\')[-1][3:-1]
+    return txt
