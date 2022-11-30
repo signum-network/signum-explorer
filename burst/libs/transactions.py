10786b5d4910f3360e64dbe3e8661aab3a264d14
pir8radio <pir8radio@gmail.com>
2022-11-30 09:05:24 -0600

    Update transactions.py
    added token message type.

--- burst/libs/transactions.py
+++ burst/libs/transactions.py
@@ -9,3 +9,9 @@ def get_message_sub(attachment: bytes) -> str:
     body = attachment[10:]
     message_len = 2 ** 8 * header[2] + header[1]
     return body[:message_len].decode()
+    
+def get_message_token(attachment: bytes) -> str:
+    header = attachment[:22]
+    body = attachment[22:]
+    message_len = 2 ** 8 * header[2] + header[1]
+    return body[:message_len].decode()
