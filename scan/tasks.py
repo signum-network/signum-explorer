d41638152572d85f2709ae3d021ed171875ce835
pir8radio <pir8radio@gmail.com>
2024-12-15 15:54:14 -0600

    Update tasks.py
    finally updated the tasks script to only update cache when needed.

--- scan/tasks.py
+++ scan/tasks.py
@@ -5,6 +5,7 @@ import requests, json
 from time import sleep
 from django.conf import settings
 from config.settings import TASKS_SCAN_DELAY, SNR_MASTER_EXPLORER
+from django.core.cache import cache
 
 from scan.models import PeerMonitor
 from scan.caching_data.exchange import CachingExchangeData
@@ -26,28 +27,41 @@ logger = logging.getLogger(__name__)
 def task_cmd():
     if TASKS_SCAN_DELAY > 0:  # Delay in env used when supervisord is used.
         logger.info(f"Tasks Sleeping for {TASKS_SCAN_DELAY} seconds...")
-        
-############# Update Exchange ############## (change to 60 sec update)
-    sleep(TASKS_SCAN_DELAY)
-    logger.info("TASK - Update Cache Exchange data")
-    CachingExchangeData().update_live_data()
-    
-############## Update Total TX ############# (match block time, heavy db request)
-    logger.info("TASK - Update Total TX's count data")
-    CachingTotalTxsCount().update_live_data()
+        sleep(TASKS_SCAN_DELAY)
+        logger.info("Checking for tasks that need to be updated.")       
 
+############# Update Exchange ############## (update exchange from api every 5 min)
+    exchange_ttl = cache.get('exchange_ttl')
+    if not exchange_ttl:
+        logger.info("  TASK - Update Cache Exchange data")
+        CachingExchangeData().update_live_data()
+        cache.set('exchange_ttl', 'ttl', timeout=300) 
+        
+############## Update Total TX ############# (match block time, heavy db request)
+    total_tx_ttl = cache.get('total_tx_ttl')
+    if not total_tx_ttl:
+        logger.info("  TASK - Update Total TX's count data")
+        CachingTotalTxsCount().update_live_data()
+        cache.set('total_tx_ttl', 'ttl', timeout=240)
+        
 ######### Update Total Circulating ######### (match block time, heavy db request)
-    logger.info("TASK - Update Total Circulating data")
-    CachingTotalCirculating().update_live_data()
+    total_circ_ttl = cache.get('total_circ_ttl')
+    if not total_circ_ttl:
+        logger.info("  TASK - Update Total Circulating data")
+        CachingTotalCirculating().update_live_data()
+        cache.set('total_circ_ttl', 'ttl', timeout=240)
     
-######### Update Peers SNR Status ########## (change to 6 hours update)
+######### Update Peers SNR Status ########## (update every 3 hours)
     if SNR_MASTER_EXPLORER :
-        logger.info("TASK - Update Peer SNR data")
-        snr_master = list(requests.get(url=SNR_MASTER_EXPLORER + "/json/SNRinfo").json())
-        for node in snr_master:
-            PeerMonitor.objects.filter(announced_address=node[0]).update(reward_state=node[2], reward_time=node[3])
-        if snr_master :
-            logger.info("SNR Master Data Received")
+        snr_master_ttl = cache.get('snr_master_ttl')
+        if not snr_master_ttl:
+            logger.info("  TASK - Update Peer SNR data")
+            snr_master = list(requests.get(url=SNR_MASTER_EXPLORER + "/json/SNRinfo").json())
+            for node in snr_master:
+                PeerMonitor.objects.filter(announced_address=node[0]).update(reward_state=node[2], reward_time=node[3])
+            if snr_master :
+                logger.info("SNR Master Data Received")
+                cache.set('snr_master_ttl', 'ttl', timeout=10800)
 
 
 
