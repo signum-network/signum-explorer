713c592b6f67fae81bf198b545df8d4efc20ccc9
frankTheTank72 <39116221+frankTheTank72@users.noreply.github.com>
2025-09-09 19:19:27 +0200

    Update config

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -5,12 +5,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 bind = "0.0.0.0:5000"
-workers = 3 * multiprocessing.cpu_count() - 1
-threads = 3 * multiprocessing.cpu_count()
-timeout = 60
-keepalive = 2
-max_requests = 500
-max_requests_jitter = 100
+worker_class = "gthread"
+workers = max(2, multiprocessing.cpu_count() * 2)
+#threads = 3 * multiprocessing.cpu_count()
+threads = 4
+timeout = 120
+graceful_timeout = 30
+keepalive = 5
+max_requests = 2000
+max_requests_jitter = 300
 worker_tmp_dir = "/dev/shm"
 forwarded_allow_ips = "*"
 proxy_allow_ips = "*"
